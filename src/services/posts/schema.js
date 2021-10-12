a4179f575732a56c5c7468949b42f4fa1befa7a4
BananaKun <yanxun951224@gmail.com>
2021-10-12 12:04:13 +0100

    edit shema

--- src/services/posts/schema.js
+++ src/services/posts/schema.js
@@ -5,21 +5,11 @@ const { Schema, model } = mongoose
 const postSchema = new Schema(
     {
         text: { type: String, required: true },
-        username: { type: String, required: true },
-        user: {
-            name: { type: String, required: true },
-            surname: { type: String, required: true },
-            email: { type: String, required: true },
-            bio: { type: String },
-            title: { type: String, required: true },
-            area: { type: String, required: true },
-            image: { type: String, default: 'https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' },
-            username: { type: String, required: true },
-        },
+        user: { type: Schema.ObjectId, ref: "profiles" },
+        image: { type: String, default: 'https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' }
     },
     {
         timestamps: true,
-        image: { type: String, default: 'https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' }
     }
 )
 
