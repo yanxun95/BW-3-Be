c5a36ffe16cc0672820838c564b9c56ccab1a914
BananaKun <yanxun951224@gmail.com>
2021-10-12 10:27:21 +0100

    update

--- src/services/profiles/pdf.js
+++ src/services/profiles/pdf.js
@@ -4,20 +4,20 @@ import PdfPrinter from "pdfmake";
 export const gettingpdfwithcontent = (data) => {
 
     const fonts = {
-        Roboto:{
+        Roboto: {
             normal: "Helvetica",
             bold: "Helvetica-bold"
         },
     }
-    
+
     const docDefination = {
         content= data,
     }
-    
+
     const printer = new PdfPrinter(fonts)
-    
-    const pdfReadableStream =printer.createPdfKitDocument(docDefination)
-    
+
+    const pdfReadableStream = printer.createPdfKitDocument(docDefination)
+
     pdfReadableStream.end()
 
 }
