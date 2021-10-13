9b861e77fd57d476c12c25600d023929bff22fa5
BananaKun <yanxun951224@gmail.com>
2021-10-13 17:25:18 +0100

    like function

--- src/services/posts/schema.js
+++ src/services/posts/schema.js
@@ -7,7 +7,7 @@ const postSchema = new Schema(
         text: { type: String, required: true },
         user: { type: Schema.ObjectId, ref: "profiles" },
         image: { type: String, default: 'https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' },
-        likes: { type: Number }
+        likes: { type: Number, default: 0 }
     },
     {
         timestamps: true,
